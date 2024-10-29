20c72812e6afbf9200110b55f3f3d30488c747ce
Jared Morrison <jrdmrrsn@gmail.com>
2024-10-29 13:42:50 -0400

    Add file download functionality to spacecomms_interface

--- MOC/spacecomms_interface/spacecomms_interface.py
+++ MOC/spacecomms_interface/spacecomms_interface.py
@@ -10,30 +10,29 @@ obc_api = FP_API_OBC()
 
 # Gets uptime from OBC through SpaceComms
 def get_uptime():
-
-    # Creating a properly formatted request using the OBC client app API
     serialized_request = list(obc_api.req_getUptime())
-
-    # Sending serialized_request and retrieving response
     serialized_response = send_command(SatelliteId.DEFAULT_ID, CommandType.OBC_FP_GATEWAY, TripType.WAIT_FOR_RESPONSE, ModuleMac.OBC_MAC_ADDRESS, payload=serialized_request)
-    
-    # Parsing response using OBC client app API
     parsed_response = obc_api.resp_getUptime(serialized_response)
-
-    # Printing s_upTime field of response to console
     logging.info(vars(parsed_response["s__upTime"]))
+
+
+# Downloads a file given a filename
+def download_file(file_name: str):
+    file_format = "{0}\0".format(file_name).encode("utf-8")
+    serialized_request = [0, 0, 0, 0, 0]
+    serialized_request.extend(file_format)
+    serialized_response = send_command(SatelliteId.DEFAULT_ID, CommandType.OBC_FILE_DOWNLOAD, TripType.WAIT_FOR_RESPONSE, ModuleMac.OBC_MAC_ADDRESS, payload=serialized_request, add_payload_length=False)
+    
+    with open(file_name, "wb") as file:
+        file.write(serialized_response)
+    
+    logging.info("File {0} written to current directory".format(file_name))
 
 
 # Initializing the radio
 def init_radio():
-
-    # Setting radio address to the UHF's MAC address
     set_radio_address(ModuleMac.UHF_MAC_ADDRESS)
-
-    # Setting uplink and downlink frequency
     update_frequency(RadioConfiguration.UHF_UPLINK_FREQUENCY, RadioConfiguration.UHF_DOWNLINK_FREQUENCY)
-
-    # Setting the encryption key
     update_aes_key(EncyptionKey.AES_IV, EncyptionKey.AES_KEY)
 
 
@@ -43,4 +42,7 @@ if __name__ == "__main__":
 
     print("Getting Uptime...")
     get_uptime()
+
+    print("Downloading dirlist...")
+    download_file("DIRLIST.TXT")
     
