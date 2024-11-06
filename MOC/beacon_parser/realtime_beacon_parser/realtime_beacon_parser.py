ceec26e1c37cc23a3d50cadba7261398d05f884c
Jared Morrison <jrdmrrsn@gmail.com>
2024-11-06 11:06:09 -0500

    Run OBC.py, grab its output

---
+++ MOC/beacon_parser/realtime_beacon_parser/realtime_beacon_parser.py
@@ -0,0 +1,16 @@
+import subprocess
+
+
+def execute(cmd):
+    popen = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+    
+
+for output_line in execute("python3, OBC.py"):
+    print(output_line)
